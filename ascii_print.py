69fe8fdb274ba33f6069bca2c643a19a8fb24c6d
olaholunuga <olaholunuga@gmail.com>
2023-11-16 12:16:27 +0100

    creat: ascii_lowercase

---
+++ ascii_print.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""print ascii letters in lowercase
+"""
+lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
+for i in lowercase_letters:
+    if i != "q":
+        print(i, end="");
